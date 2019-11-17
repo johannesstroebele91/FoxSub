64b95261c031eabea5b04834b4b36e2804878a69
Antoniadis Rafail <ra022@hdm-stuttgart.de>
2019-11-17 23:51:29 +0100

    fix boostrapModule

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,7 @@
 import './polyfills';
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-platformBrowserDynamic().bootstrapMosdule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
   // eslint-disable-next-line no-console
   .catch((err) => console.error(err));
