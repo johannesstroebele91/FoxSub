b86f539a93b0db121df18e49e8e0a0d4b8e68340
Johannes91 <stroebele.j@gmail.com>
2019-11-29 15:14:40 +0100

    Setup subscription component

---
+++ client/src/app/subscriptions/list/subscriptions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'subscriptions',
+    templateUrl: './subscriptions.component.html',
+    styleUrls: ['./subscriptions.component.scss']
+})
+export class SubscriptionsComponent { }
