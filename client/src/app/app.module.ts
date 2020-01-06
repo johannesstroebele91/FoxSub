b990bea14ec9bf304bb6070e812d04feab46f32e
JohannesStroebele <stroebele.j@gmail.com>
2020-01-06 15:01:31 +0100

    Setup basic login component and fixed naming of subscription-list, subscription-list element

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent} from "./header/header.component";
 import { SubscriptionListComponent } from './subscriptions/list/subscription-list.component';
 import { SubscriptionListElementComponent} from "./subscriptions/list/expansion/subscription-list-element.component";
-import { HeaderComponent} from "./header/header.component";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
@@ -15,9 +16,10 @@ import { HeaderComponent} from "./header/header.component";
   ],
   declarations: [
     AppComponent,
+    HeaderComponent,
     SubscriptionListComponent,
     SubscriptionListElementComponent,
-    HeaderComponent
+    LoginComponent
   ],
   bootstrap: [AppComponent]
 })
