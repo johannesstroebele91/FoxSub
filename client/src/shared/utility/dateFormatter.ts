d233c4805fe1cba46cfffd5c435c6ed41c3bba5d
Cuntz Fabian <fc016@hdm-stuttgart.de>
2020-01-23 19:00:18 +0100

    date function die erste

---
+++ client/src/shared/utility/dateFormatter.ts
@@ -0,0 +1,16 @@
+export class DateFormatter {
+
+    constructor() { }
+
+    formatDateToDB(date: string) {
+        let newDate = new Date(date);
+        console.log(newDate);
+        //let todayDate = new Date(Date.parse(Date()));
+       // dueDate:  = { day: <day>, month: <month>}
+    }
+
+    formatDateFromDB(date: string) {
+
+    }
+
+}
