0c539d4a43d97c59d72ea14585248f42dbc34b06
JohannesStroebele <stroebele.j@gmail.com>
2020-01-23 15:43:07 +0100

    Fixed docker files

--- server/db.go
+++ server/db.go
@@ -8,7 +8,7 @@ func CreateDBConnection() *sqlx.DB {
 	var db *sqlx.DB
 	var err error
 
-	db, err = sqlx.Connect("mysql", "root:password@tcp(127.0.0.1:3306)/fabulous-fox-dev")
+	db, err = sqlx.Connect("mysql", "root:password@tcp(127.0.0.1:3306)/fabulous-fox")
 
 	if err != nil {
 		log.Println("Can't establish connection to maria db", err)
